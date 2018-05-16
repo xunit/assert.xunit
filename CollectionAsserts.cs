xunit/assert.xunit

commit c17d92d642b75b1acae4d59e1dc05276d50689c7

Performance tweak for Assert.Single: Early terminate if possible.

CollectionAsserts.cs
@@ -345,12 +345,26 @@ namespace Xunit
             int count = 0;
             T result = default(T);
 
-            foreach (T item in collection)
-                if (predicate(item))
+            T item;
+            var enumerator = collection.GetEnumerator();
+            try
+            {
+                while(enumerator.MoveNext())
                 {
-                    result = item;
-                    ++count;
+                    item = enumerator.Current;
+                    if (predicate(item))
+                    {
+                        ++count;
+                        if (count > 1)
+                            break;
+                        result = item;
+                    }
                 }
+            }
+            finally
+            {
+                (enumerator as IDisposable)?.Dispose();
+            }
 
             if (count != 1)
                 throw new SingleException(count);

commit d68edc3d922e5703fae75244a14d8d156711a31b

Fixed error message of SingleException (broken by my optimization in c17d92d642b75b1acae4d59e1dc05276d50689c7)

CollectionAsserts.cs
@@ -366,8 +366,12 @@ namespace Xunit
                 (enumerator as IDisposable)?.Dispose();
             }
 
-            if (count != 1)
-                throw new SingleException(count);
+            switch (count)
+            {
+                case 0: throw SingleException.Empty();
+                case 1: break;
+                default: throw SingleException.MoreThanOne();
+            }
 
             return result;
         }

Sdk/Exceptions/SingleException.cs
@@ -8,12 +8,17 @@
 #else
     public
 #endif
-    class SingleException : AssertCollectionCountException
+    class SingleException : XunitException
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="SingleException"/> class.
         /// </summary>
-        /// <param name="count">The numbers of items in the collection.</param>
-        public SingleException(int count) : base(1, count) { }
+        private SingleException(string errorMessage) : base(errorMessage) { }
+        
+        public static SingleException Empty() =>
+            new SingleException("The collection was expected to contain a single element, but it was empty.");
+        
+        public static SingleException MoreThanOne() =>
+            new SingleException("The collection was expected to contain a single element, but it contained more than one element.");
     }
 }