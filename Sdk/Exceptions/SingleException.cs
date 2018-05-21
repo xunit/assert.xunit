9e628f73be865af5f69f11af4ee2d2edd8030af0
Brad Wilson <dotnetguy@gmail.com>
2018-05-20 18:08:41 -0700

    Spacing

--- Sdk/Exceptions/SingleException.cs
+++ Sdk/Exceptions/SingleException.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// Exception thrown when the collection did not contain exactly one element.
     /// </summary>
-#if XUNIT_VISIBILITY_INTERNAL 
+#if XUNIT_VISIBILITY_INTERNAL
     internal
 #else
     public
@@ -14,10 +14,10 @@
         /// Initializes a new instance of the <see cref="SingleException"/> class.
         /// </summary>
         private SingleException(string errorMessage) : base(errorMessage) { }
-        
+
         public static SingleException Empty() =>
             new SingleException("The collection was expected to contain a single element, but it was empty.");
-        
+
         public static SingleException MoreThanOne() =>
             new SingleException("The collection was expected to contain a single element, but it contained more than one element.");
     }
