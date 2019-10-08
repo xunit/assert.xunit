24d3c79feeff30c4115a5897d4153d402d36d479
Jostein Kjønigsen <jostein@kjonigsen.net>
2019-10-08 09:48:31 +0200

    Add Assert.Fail() operation. This partially resolves https://github.com/xunit/xunit/issues/2027.

---
+++ FailAsserts.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit.Sdk;
+
+namespace Xunit
+{
+#if XUNIT_VISIBILITY_INTERNAL
+    internal
+#else
+    public
+#endif
+    partial class Assert
+    {
+        /// <summary>
+        /// Explicitly fail a test.
+        /// </summary>
+        public static void Fail()
+        {
+            throw new FailException();
+        }
+
+        /// <summary>
+        /// Explicitly fail a test.
+        /// </summary>
+        /// <param name="userMessage">The message to show as reason for failure.</param>
+        public static void Fail(string userMessage)
+        {
+            throw new FailException(userMessage);
+        }
+    }
+}
---
+++ Sdk/Exceptions/FailException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xunit.Sdk
+{
+    /// <summary>
+    /// Exception thrown when a test is explicitly failed.
+    /// </summary>
+#if XUNIT_VISIBILITY_INTERNAL
+    internal
+#else
+    public
+#endif
+    class FailException : XunitException
+    {
+        /// <summary>
+        /// Creates a new instance of the <see cref="FailException"/> class.
+        /// </summary>
+        public FailException()
+            :this("Assert.Fail() Failure")
+        { }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="FailException"/> class.
+        /// </summary>
+        /// <param name="userMessage">The message to show as reason for failure.</param>
+        public FailException(string userMessage)
+            : base(userMessage)
+        { }
+    }
+}
