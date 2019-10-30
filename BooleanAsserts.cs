d57c3af1f8d05020507e1dbaa15313e9db5e379a
Drew Noakes <git@drewnoakes.com>
2019-10-30 20:37:03 +1100

    Annotate BooleanAsserts

--- BooleanAsserts.cs
+++ BooleanAsserts.cs
@@ -1,4 +1,7 @@
-﻿using Xunit.Sdk;
+﻿#nullable enable
+
+using System.Diagnostics.CodeAnalysis;
+using Xunit.Sdk;
 
 namespace Xunit
 {
@@ -14,7 +17,7 @@ namespace Xunit
         /// </summary>
         /// <param name="condition">The condition to be tested</param>
         /// <exception cref="FalseException">Thrown if the condition is not false</exception>
-        public static void False(bool condition)
+        public static void False([DoesNotReturnIf(parameterValue: true)] bool condition)
         {
             False((bool?)condition, null);
         }
@@ -24,7 +27,7 @@ namespace Xunit
         /// </summary>
         /// <param name="condition">The condition to be tested</param>
         /// <exception cref="FalseException">Thrown if the condition is not false</exception>
-        public static void False(bool? condition)
+        public static void False([DoesNotReturnIf(parameterValue: true)] bool? condition)
         {
             False(condition, null);
         }
@@ -35,7 +38,7 @@ namespace Xunit
         /// <param name="condition">The condition to be tested</param>
         /// <param name="userMessage">The message to show when the condition is not false</param>
         /// <exception cref="FalseException">Thrown if the condition is not false</exception>
-        public static void False(bool condition, string userMessage)
+        public static void False([DoesNotReturnIf(parameterValue: true)] bool condition, string? userMessage)
         {
             False((bool?)condition, userMessage);
         }
@@ -46,7 +49,7 @@ namespace Xunit
         /// <param name="condition">The condition to be tested</param>
         /// <param name="userMessage">The message to show when the condition is not false</param>
         /// <exception cref="FalseException">Thrown if the condition is not false</exception>
-        public static void False(bool? condition, string userMessage)
+        public static void False([DoesNotReturnIf(parameterValue: true)] bool? condition, string? userMessage)
         {
             if (!condition.HasValue || condition.GetValueOrDefault())
                 throw new FalseException(userMessage, condition);
@@ -57,7 +60,7 @@ namespace Xunit
         /// </summary>
         /// <param name="condition">The condition to be inspected</param>
         /// <exception cref="TrueException">Thrown when the condition is false</exception>
-        public static void True(bool condition)
+        public static void True([DoesNotReturnIf(parameterValue: false)] bool condition)
         {
             True((bool?)condition, null);
         }
@@ -67,7 +70,7 @@ namespace Xunit
         /// </summary>
         /// <param name="condition">The condition to be inspected</param>
         /// <exception cref="TrueException">Thrown when the condition is false</exception>
-        public static void True(bool? condition)
+        public static void True([DoesNotReturnIf(parameterValue: false)] bool? condition)
         {
             True(condition, null);
         }
@@ -78,7 +81,7 @@ namespace Xunit
         /// <param name="condition">The condition to be inspected</param>
         /// <param name="userMessage">The message to be shown when the condition is false</param>
         /// <exception cref="TrueException">Thrown when the condition is false</exception>
-        public static void True(bool condition, string userMessage)
+        public static void True([DoesNotReturnIf(parameterValue: false)] bool condition, string? userMessage)
         {
             True((bool?)condition, userMessage);
         }
@@ -89,7 +92,7 @@ namespace Xunit
         /// <param name="condition">The condition to be inspected</param>
         /// <param name="userMessage">The message to be shown when the condition is false</param>
         /// <exception cref="TrueException">Thrown when the condition is false</exception>
-        public static void True(bool? condition, string userMessage)
+        public static void True([DoesNotReturnIf(parameterValue: false)] bool? condition, string? userMessage)
         {
             if (!condition.HasValue || !condition.GetValueOrDefault())
                 throw new TrueException(userMessage, condition);
