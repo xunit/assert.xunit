dd2704a1aa88e40b86a404a6486b850421ef98e3
Brad Wilson <dotnetguy@gmail.com>
2023-05-07 16:56:06 -0700

    Roll back to C# 10.0

--- Sdk/CollectionTrackerExtensions.cs
+++ Sdk/CollectionTrackerExtensions.cs
@@ -13,7 +13,7 @@ namespace Xunit.Sdk
 	static class CollectionTrackerExtensions
 	{
 #if XUNIT_NULLABLE
-		[return: NotNullIfNotNull(nameof(enumerable))]
+		[return: NotNullIfNotNull("enumerable")]
 		public static CollectionTracker<object>? AsTracker(this IEnumerable? enumerable) =>
 #else
 		public static CollectionTracker<object> AsTracker(this IEnumerable enumerable) =>
@@ -23,7 +23,7 @@ namespace Xunit.Sdk
 				: enumerable as CollectionTracker<object> ?? CollectionTracker<object>.Wrap(enumerable.Cast<object>());
 
 #if XUNIT_NULLABLE
-		[return: NotNullIfNotNull(nameof(enumerable))]
+		[return: NotNullIfNotNull("enumerable")]
 		public static CollectionTracker<T>? AsTracker<T>(this IEnumerable<T>? enumerable) =>
 #else
 		public static CollectionTracker<T> AsTracker<T>(this IEnumerable<T> enumerable) =>
