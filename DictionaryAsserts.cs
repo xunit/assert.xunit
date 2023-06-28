229d2e631d21779d0120e99f02b5666c3c885700
Brad Wilson <dotnetguy@gmail.com>
2023-06-28 12:41:42 -0700

    xunit/xunit#1857: Add overload to Assert.Contains for ConcurrentDictionary

--- DictionaryAsserts.cs
+++ DictionaryAsserts.cs
@@ -6,6 +6,7 @@
 #pragma warning disable CS8714
 #endif
 
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using Xunit.Sdk;
@@ -79,6 +80,25 @@ namespace Xunit
 				);
 
 			return value;
+		}
+
+		/// <summary>
+		/// Verifies that a dictionary contains a given key.
+		/// </summary>
+		/// <typeparam name="TKey">The type of the keys of the object to be verified.</typeparam>
+		/// <typeparam name="TValue">The type of the values of the object to be verified.</typeparam>
+		/// <param name="expected">The object expected to be in the collection.</param>
+		/// <param name="collection">The collection to be inspected.</param>
+		/// <returns>The value associated with <paramref name="expected"/>.</returns>
+		/// <exception cref="ContainsException">Thrown when the object is not present in the collection</exception>
+		public static TValue Contains<TKey, TValue>(
+			TKey expected,
+			ConcurrentDictionary<TKey, TValue> collection)
+#if XUNIT_NULLABLE
+				where TKey : notnull
+#endif
+		{
+			return Contains(expected, (IReadOnlyDictionary<TKey, TValue>)collection);
 		}
 
 		/// <summary>
@@ -202,12 +222,30 @@ namespace Xunit
 		/// <exception cref="DoesNotContainException">Thrown when the object is present in the collection</exception>
 		public static void DoesNotContain<TKey, TValue>(
 			TKey expected,
+			ConcurrentDictionary<TKey, TValue> collection)
+#if XUNIT_NULLABLE
+				where TKey : notnull
+#endif
+		{
+			DoesNotContain(expected, (IReadOnlyDictionary<TKey, TValue>)collection);
+		}
+
+		/// <summary>
+		/// Verifies that a dictionary does not contain a given key.
+		/// </summary>
+		/// <typeparam name="TKey">The type of the keys of the object to be verified.</typeparam>
+		/// <typeparam name="TValue">The type of the values of the object to be verified.</typeparam>
+		/// <param name="expected">The object expected to be in the collection.</param>
+		/// <param name="collection">The collection to be inspected.</param>
+		/// <exception cref="DoesNotContainException">Thrown when the object is present in the collection</exception>
+		public static void DoesNotContain<TKey, TValue>(
+			TKey expected,
 			Dictionary<TKey, TValue> collection)
 #if XUNIT_NULLABLE
 				where TKey : notnull
 #endif
 		{
-			DoesNotContain(expected, (IDictionary<TKey, TValue>)collection);
+			DoesNotContain(expected, (IReadOnlyDictionary<TKey, TValue>)collection);
 		}
 
 		/// <summary>
