001dc3369c73ad3b5fdef4905f2c6c44f6b1b897
Brad Wilson <dotnetguy@gmail.com>
2024-07-05 13:22:39 -0700

    Fixup incorrect field reference

--- Sdk/IAssertionException.cs
+++ Sdk/IAssertionException.cs
@@ -8,7 +8,7 @@ namespace Xunit.Sdk
 {
 	/// <summary>
 	/// This is a marker interface implemented by all built-in assertion exceptions so that
-	/// test failures can be marked with <see cref="F:Xunit.v3.FailureCause.Assertion"/>.
+	/// test failures can be marked with <see cref="F:Xunit.Sdk.FailureCause.Assertion"/>.
 	/// </summary>
 #if XUNIT_VISIBILITY_INTERNAL
 	internal
